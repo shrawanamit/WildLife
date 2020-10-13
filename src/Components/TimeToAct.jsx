f267ef58b3242a0c56773fe16200682be35a6d1b
“shrawanamit” <amitkumar06111@gmail.com>
2020-10-13 12:28:33 +0530

    Refactored Time to act container

--- src/Components/TimeToAct.jsx
+++ src/Components/TimeToAct.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import '../SCSS/timeToAct.scss';
-import bgColor from '../assets/bg-image.jpg'
+import Button from 'react-bootstrap/Button'
+
 export default class TimeToAct extends React.Component {
     render() {
         return (
-            <div className="TimeToAct">
-                <div className="TimeToActBody">
-                    <img src={bgColor} className="TimeToActImage" />
-                   
+            <div className="TimeToActContainer">
+
+                <div className="timeToAct">
+                    <span className="timeToActHeading">IT'S </span>
+                    <span className="timeToActTime"> TIME</span>
+                    <span className="timeToActHeading"> TO ACT</span>
                 </div>
-                {/* <div className="timeToAct">IT'S TIME TO ACT</div> */}
+                <div className="readMore">
+                    <Button variant="warning" size="lg">Read More</Button>{' '}
+                </div>
 
             </div>
         );
