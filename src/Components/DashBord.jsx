ec1c83cf9ee744c10bd0d346c00dfeabbcba74b4
“shrawanamit” <amitkumar06111@gmail.com>
2020-10-14 09:01:32 +0530

    Added code of side Nav

--- src/Components/DashBord.jsx
+++ src/Components/DashBord.jsx
@@ -11,27 +11,50 @@ import FormDonate from './FormDonate';
 import HelpAnimal from './HelpAnimal'
 import TimeToAct from './TimeToAct'
 import SavedAnimal from './SavedAnimal'
-import  AboutWildLife from  './AboutWildLife';
+import AboutWildLife from './AboutWildLife';
 import Footer from './Footer'
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 
 export default class DashBoard extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: true,
+        }
+    }
+    handelDrawer = () => {
+        this.setState({ open: !this.state.open });
+    }
     render() {
         return (
             <div className='dashBoardContainer'>
                 <div className='toolBarContainer'>
                     <div className="toolbar">
                         <div className="toolbarHeading">
-                            <img src={logo}  className="logo"/>
+                            <img src={logo} className="logo" />
                         </div>
                         <div className="toolbarcontent">
-                            <spam>ABOUT US</spam>
-                            <spam>PROJECT</spam>
-                            <spam>DONATE</spam>
-                             <FacebookIcon />
-                             <InstagramIcon />
-                             <TwitterIcon />
-                             <YouTubeIcon />
-                             <MenuIcon />
+                            <spam className="aboutUs">ABOUT US</spam>
+                            <spam  className="aboutUs">PROJECT</spam>
+                            <spam  className="aboutUs">DONATE</spam>
+                            <FacebookIcon />
+                            <InstagramIcon />
+                            <TwitterIcon />
+                            <YouTubeIcon />
+                            <div  className="toggelSideNav" onClick={this.handelDrawer}>
+                                {this.state.open ? <MenuIcon /> : <ArrowBackIcon />}
+                                {this.state.open ?"":<div className="sideNav">
+
+                                    <spam className="home">HOME</spam>
+                                    <spam  className="home">PAGES</spam>
+                                    <spam  className="home">OUR TEAM</spam>
+                                    <spam  className="home">PARTNERS</spam>
+                                    <spam  className="home">CONTACTS</spam>
+                                    <spam  className="home"></spam>
+                                </div>}
+                            </div>
+
                         </div>
                     </div>
                 </div>
