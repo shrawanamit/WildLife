aa823bfcf31c1a322257f3a1041d35bc6ba72449
“shrawanamit” <amitkumar06111@gmail.com>
2020-10-19 18:33:22 +0530

    added code lazy loading component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React ,{lazy,Suspense}from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import DashBoard from './Components/DashBord'
+// import DashBoard from './Components/DashBord';
+const DashBoard = lazy(() => import('./Components/DashBord'));
+
 function App() {
   return (
+    <Suspense fallback={<div>Loading ...</div>}>
     <div className="App">
       <Router>
         <Route exact path="/" component={DashBoard} />
       </Router>
     </div>
+    </Suspense>
   );
 }
 
